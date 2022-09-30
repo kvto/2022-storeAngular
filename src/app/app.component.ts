6e1f479a2fdb4fce748ddf742c0dac22ba723049
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-09-30 17:29:15 -0500

    kev - 2022-store-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'sJunior el mas bello';
+
+  getName(): void{
+    console.log("holaaaaa")
+  }
 }
