85d52002f5714cf71e2153dbf161fe57c678c48e
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-11-06 21:37:02 -0500

    kev-2022-storeAngular

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -22,6 +22,6 @@ export class DataService{
     }
 
     saveDetailsOrder(details:DetailsOrder):Observable<DetailsOrder>{
-        return this.http.post<DetailsOrder>(`${this.apiURL}/datailsOrders`, details);
+        return this.http.post<DetailsOrder>(`${this.apiURL}/detailsOrders`, details);
     }
 }
